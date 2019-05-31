366a4bf8bf4a3d49c6ef270f69c920ad79f7f09c
Thiago <thiagoximeneslima@gmail.com>
2019-05-31 13:57:31 -0300

    Falta fazer o post do aquivo Json

--- request.py
+++ request.py
@@ -1,4 +1,8 @@
 import requests
+import json
+d = {'numero_casas': 4, 'token': '36c185a3c5f023761c4fb212e769a2de24f747dc', 'cifrado': 'glygo rsvvmw hsiwr’x ks lyrxmrk. glygo rsvvmw ksiw omppmrk. yrorsar', 'decifrado': 'chuck norris doesn’t go hunting. chuck norris goes killing. unknown', 'resumo_criptografico': 'fc1502438d6682ea6779622b43f6c37a3d0e44eb'}
+rs = requests.post('https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token=36c185a3c5f023761c4fb212e769a2de24f747dc', data=d)
+print(rs.text)
 r = requests.get('https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=36c185a3c5f023761c4fb212e769a2de24f747dc')
 print(r.status_code)
-print(r.text)
+print(r.json())
